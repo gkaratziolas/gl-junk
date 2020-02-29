fb059f1282819b6d380be0f19a6897faa37d471b
Giorgos Karatziolas <gkaratziolas@gmail.com>
2020-02-29 15:44:12 +0000

    Add some more colour map options to fragment shader

--- turing-patterns/texture.fs
+++ turing-patterns/texture.fs
@@ -6,8 +6,44 @@ in vec2 TexCoord;
 // texture sampler
 uniform sampler2D texture1;
 
+vec4 linear_colour(float x)
+{
+	float r = 1.0f - x;
+	float b = x;
+	float g;
+	if (x < 0.5) {
+		g = 4.0f * x - 1.0f;
+	} else {
+		g = -4.0f * x - 3.0f;
+	}
+	if (g < 0.0f) {
+		g = 0.0f;
+	}
+	return vec4(r, g, b, 1.0f);	
+}
+
+vec4 leopard(float x)
+{
+	float r = 0.18039f + x * 0.74117f;
+	float g = 0.14901f + x * 0.52941f;
+	float b = 0.06667f;
+
+	return vec4(r, g, b, 1.0f);
+}
+
+vec4 constant_sum_colour(float x)
+{
+	float pi = 3.1415926f;
+	
+	x = 1.0f - x;
+	float r = (sin(pi*x) + 1.0f) / 2.0f;
+	float g = (sin(pi*(x + 2.0f/3.0f)) + 1.0f) / 2.0f;
+	float b = (sin(pi*(x + 4.0f/3.0f)) + 1.0f) / 2.0f;
+	return vec4(r, g, b, 1.0f);
+}
+
 void main()
 {
-	vec4 TexColor = texture(texture1, TexCoord);
-	FragColor = vec4(1.0f - TexColor.x, 0.0f, TexColor.x, 1.0f);
+	float intensity = texture(texture1, TexCoord).x;
+	FragColor = constant_sum_colour(intensity);
 }
