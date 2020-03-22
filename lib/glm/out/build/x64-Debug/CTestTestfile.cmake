3886a9a062ec97ffffb0d45e3d7513f174110890
Giorgos Karatziolas <gkaratziolas@gmail.com>
2020-03-22 08:55:38 +0000

    Move libraries to different folder

---
+++ lib/glm/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/meisg/source/repos/gl-junk/glm
+# Build directory: C:/Users/meisg/source/repos/gl-junk/glm/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
---
+++ lib/glm/out/build/x64-Debug/test/gtc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/meisg/source/repos/gl-junk/glm/test/gtc
+# Build directory: C:/Users/meisg/source/repos/gl-junk/glm/out/build/x64-Debug/test/gtc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
