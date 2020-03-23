7a7fb42433ecf582a4fb5d54a3715f5e5dd14566
Giorgos Karatziolas <gkaratziolas@gmail.com>
2020-03-23 22:10:14 +0000

    Tidy up some glm stuff

--- lib/glm/out/build/x64-Debug/CTestTestfile.cmake
+++ lib/glm/out/build/x64-Debug/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/meisg/source/repos/gl-junk/glm
-# Build directory: C:/Users/meisg/source/repos/gl-junk/glm/out/build/x64-Debug
+# Source directory: C:/Users/meisg/source/repos/gl-junk/lib/glm
+# Build directory: C:/Users/meisg/source/repos/gl-junk/lib/glm/out/build/x64-Debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
